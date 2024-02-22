9a22bd2591f5a5988400252da1d3352285aa4cb0
kamal0620040 <neupane0620040@outlook.com>
2024-02-22 12:04:09 +0545

    Added email and password validation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,20 @@ const express = require("express");
 
 const { InfoController } = require("../../controllers");
 const { UserController } = require("../../controllers/index");
+const { AuthRequestValidators } = require("../../middlewares/index");
 
 const router = express.Router();
 
 router.get("/info", InfoController.info);
-router.post("/signup", UserController.create);
-router.post("/signin", UserController.signIn);
+router.post(
+  "/signup",
+  AuthRequestValidators.validateUserAuth,
+  UserController.create
+);
+router.post(
+  "/signin",
+  AuthRequestValidators.validateUserAuth,
+  UserController.signIn
+);
 
 module.exports = router;
