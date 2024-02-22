4c2567f8fef4230953dc30453d2507418f885597
kamal0620040 <neupane0620040@outlook.com>
2024-02-22 09:57:59 +0545

    Added get user by ID in repository layer

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,28 +1,40 @@
-const { User } = require('../models/index');
+const { User } = require("../models/index");
 
-class UserRepository{
-    async create(data){
-        try {
-            const user = await User.create(data);
-            return user;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw {error};
-        }
+class UserRepository {
+  async create(data) {
+    try {
+      const user = await User.create(data);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
     }
+  }
 
-    async destroy(userId){
-        try{
-            await User.destroy({
-                where:{
-                    id: userId  
-                }
-            });
-        } catch(error){
-            console.log("Something went wrong in the repository layer.");
-            throw {error};
-        }
+  async destroy(userId) {
+    try {
+      await User.destroy({
+        where: {
+          id: userId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went wrong in the repository layer.");
+      throw { error };
     }
+  }
+
+  async getByID(userId) {
+    try {
+      const user = await User.findByPk(userId, {
+        attributes: ["email", "id"],
+      });
+      return user;
+    } catch (error) {
+      console.log("Somethigng went wrong in the user repositiory layer.");
+      throw { error };
+    }
+  }
 }
 
 module.exports = UserRepository;
